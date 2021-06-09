d5c7a028b4774a3ab2211f3b8d409103d196c5da
hungvngcd18770 <hungvngcd18770@fpt.edu.vn>
2021-06-09 11:33:07 +0700

    Migration Category Table

---
+++ ASPDevApp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace ASPDevApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ASPDevApp.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ASPDevApp.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
