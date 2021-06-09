ca773cd5fe6125d97cb629f95d9cc9eca83ea791
Tran Dang Quang <Tran Dang Quang@DESKTOP-KM3ADUQ>
2021-06-09 19:42:10 +0700

    Add Course model

--- ASPDevApp/Models/Course.cs
+++ ASPDevApp/Models/Course.cs
@@ -7,6 +7,7 @@ namespace ASPDevApp.Models
 {
     public class Course
     {
+        public int Id { get; set; }
         public string CourseName { get; set; }
         public string Description { get; set;}
         public string CategoryName { get; set; }
