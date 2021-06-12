e220a8f91248caf861a25aa895ca2dd0218d7f01
hungvngcd18770 <hungvngcd18770@fpt.edu.vn>
2021-06-12 22:12:13 +0700

    Linking object Course to Category

--- ASPDevApp/Models/Course.cs
+++ ASPDevApp/Models/Course.cs
@@ -8,9 +8,10 @@ namespace ASPDevApp.Models
     public class Course
     {
         public int Id { get;set;}
-        public string CourseName { get; set; }
+        public string Name { get; set; }
         public string Description { get; set;}
-        public string CategoryName { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
 
 
     }
