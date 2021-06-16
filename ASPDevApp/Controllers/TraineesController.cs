0b609499ae97dbfaa6a28ef99747e1a9404d0241
Tran Dang Quang <Tran Dang Quang@DESKTOP-KM3ADUQ>
2021-06-16 13:10:21 +0700

    implement index  action with context

--- ASPDevApp/Controllers/TraineesController.cs
+++ ASPDevApp/Controllers/TraineesController.cs
@@ -9,33 +9,19 @@ namespace ASPDevApp.Controllers
 {
     public class TraineesController : Controller
     {
-        private List<TraineeProfile> _traineeProfiles = new List<TraineeProfile >();
+        private ApplicationDbContext _context;
+         
         
         public TraineesController()
         {
-            _traineeProfiles.Add(new TraineeProfile()
-            {
-                TraineeId = 1,
-                FristName = "Quang",
-                LastName = "Tran",
-                PhoneNumber = 12312412,
-                Gmail = "trandangquang@gmail.com",
-            });
-
-            _traineeProfiles.Add(new TraineeProfile()
-            {
-                TraineeId = 2,
-                FristName = "Hung",
-                LastName = "Kim",
-                PhoneNumber = 12444555,
-                Gmail = "kimvongochung@gmail.com",
-                
-            });
+            _context = new ApplicationDbContext();
+           
         }
         // GET: Trainees
         public ActionResult Index()
         {
-            return View(_traineeProfiles);
+            var TraineeProfiles = _context.TraineeProfiles.ToList();
+            return View(TraineeProfiles);
         }
         public ActionResult Create()
         {
