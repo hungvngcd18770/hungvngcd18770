ff6e46d262289ded9a8a45d9cf674c8c58d15da6
hungvngcd18770 <hungvngcd18770@fpt.edu.vn>
2021-06-17 21:06:25 +0700

    Add List Trainer and List Trainee

---
+++ ASPDevApp/ViewModels/ListTrainerCoursesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ASPDevApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPDevApp.ViewModels
+{
+    public class ListTrainerCoursesViewModel
+    {
+        public Course Course { get; set; }
+        public IEnumerable<TrainerProfile> TrainerProfiles { get; set; }
+    }
+}
