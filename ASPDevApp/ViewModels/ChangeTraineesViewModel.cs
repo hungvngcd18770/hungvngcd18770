883af17b0b3420de01dce4d6e95aa9ccb6f5d8f1
hungvngcd18770 <hungvngcd18770@fpt.edu.vn>
2021-06-17 17:24:28 +0700

    Add View model ChangeTrainee and ChangeTrainers

---
+++ ASPDevApp/ViewModels/ChangeTraineesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ASPDevApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPDevApp.ViewModels
+{
+    public class ChangeTraineesViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+        public TraineeProfile TraineeProfile { get; set; }
+    }
+}
---
+++ ASPDevApp/ViewModels/ChangeTrainersViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ASPDevApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPDevApp.ViewModels
+{
+    public class ChangeTrainersViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+        //  public IEnumerable<Category> Courses { get; set; }
+        public TrainerProfile TrainerProfile { get; set; }
+    }
+}
