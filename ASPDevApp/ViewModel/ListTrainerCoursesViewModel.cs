1281f92819eace390e5adf89a810560f66ac4ffa
hungvngcd18770 <hungvngcd18770@fpt.edu.vn>
2021-06-19 10:26:50 +0700

    Add listtrainer Courses

---
+++ ASPDevApp/ViewModel/ListTrainerCoursesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ASPDevApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPDevApp.ViewModel
+{
+    public class ListTrainerCoursesViewModel
+    {
+        public Course Course { get; set; }
+        public IEnumerable<TrainerProfile> TrainerProfiles { get; set; }
+    }
+}
