eef052d612b8d45830df8083fc5ea1fef96af5fe
hungvngcd18770 <hungvngcd18770@fpt.edu.vn>
2021-06-19 13:51:15 +0700

    Add index Và Tạo DecentralizationTrainerController sự dụng DBcontext

---
+++ ASPDevApp/Controllers/DecentralizationTrainerController.cs
@@ -0,0 +1,23 @@
+﻿using ASPDevApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPDevApp.Controllers
+{
+    public class DecentralizationTrainerController : Controller
+    {
+        private ApplicationDbContext _context;
+        public DecentralizationTrainerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: DecentralizationTrainer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
