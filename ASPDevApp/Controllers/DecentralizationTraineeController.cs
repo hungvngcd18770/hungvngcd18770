c846df3ae0b006bcb05a77ed23c83e6d172b0c5e
hungvngcd18770 <hungvngcd18770@fpt.edu.vn>
2021-06-19 09:59:07 +0700

    add Chỉ định trainee và Index   DecentralizationTraineeControlle

---
+++ ASPDevApp/Controllers/DecentralizationTraineeController.cs
@@ -0,0 +1,34 @@
+﻿using ASPDevApp.Models;
+using ASPDevApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPDevApp.Controllers
+{
+    public class DecentralizationTraineeController : Controller
+    {
+        // GET: DecentralizationTrainee
+        private ApplicationDbContext _context;
+
+        public DecentralizationTraineeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index(int id)
+        {
+            var traineeInCourse = _context.TraineeProfiles.Where(c => c.CourseId == id).ToList();
+            var traineeList = new ListTraineeCoursesViewModel
+            {
+                TraineeProfiles = traineeInCourse,
+                Course = _context.Courses.SingleOrDefault(c => c.Id == id)
+            };
+            return View(traineeList);
+        }
+
+
+    }
+}
---
+++ ASPDevApp/ViewModel/CourseCategoriesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ASPDevApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPDevApp.ViewModel
+{
+    public class CourseCategoriesViewModel
+    {
+        public Course Course { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
